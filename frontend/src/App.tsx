376bbe2b2a4d7981e8c29767ed3d1bb94ac90111
bnmanish <bnmanish006@gmail.com>
2026-06-10 23:27:32 +0530

    home page changes in progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import Meetings from "./pages/Meetings";
+import Home from "./front/Home";
 
 import AdminLayout from "./pages/AdminLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -11,16 +12,11 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute>
-            <AdminLayout />
-          </ProtectedRoute>
-        }
-      >
+
+      <Route path="/admin" element={ <ProtectedRoute> <AdminLayout /> </ProtectedRoute> }>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="users" element={<Users />} />
         <Route path="meetings" element={<Meetings />} />
